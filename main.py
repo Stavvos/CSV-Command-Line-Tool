08d6874d2d1daabab1723435eda9d10b32abb7ad
Liam Henry <liam_burridgeuc@hotmail.com>
2021-11-24 19:29:50 +1100

    Renamed Records class to record. Edited comments in the main.py and test_main.py files.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#Written by Liam Henry 24/11/2021
 import csv 
 
-class Records:
+class Record:
 	def __init__(self, firstname, lastname, date, division, points, summary):
 		self.firstname = firstname
 		self.lastname = lastname 
@@ -9,12 +10,12 @@ class Records:
 		self.points = int(points)
 		self.summary = summary
 	
-	#A function that formats the output of a Records object to the desired YAML formatting. 
+	#A function that formats the command line output of a Record object to the desired YAML formatting. 
 	def __str__(self):
 		return '- name: {self.firstname} {self.lastname} \n  details: In division {self.division} from {self.date} performing {self.summary}'.format(self = self) 
 
-#A function that reads a CSV file, assigns values to Records object attributes,
-#stores Records objects into an array, and returns the array filled with Records objects
+#A function that reads a CSV file, assigns values to Record object attributes,
+#stores Record objects into an array, and returns the array filled with Record objects
 def CSV(array):
 	
 	with open ('F:/CSV_Tool/CSV.txt') as CSVfile:  #Place file path of CSV.txt inbetween the parenthesis 
@@ -24,7 +25,7 @@ def CSV(array):
 			
 		for row in reader: 
 			if line_count > 0: 
-				array.append(Records(row[0],row[1],row[2],int(row[3]),int(row[4]),row[5])) 
+				array.append(Record(row[0],row[1],row[2],int(row[3]),int(row[4]),row[5])) 
 				
 			if line_count == 10:
 				break
@@ -38,7 +39,7 @@ def sortArray(array):
 	array = sorted(array, key = lambda x: (x.division, -x.points))
 	return array 
 
-#A function that displays the top 3 indexs within the given array. 
+#A function that displays the top 3 indices within the given array. 
 def display (array):
 	for i in range(3):
 		print (array[i])
--- test_main.py
+++ test_main.py
@@ -1,15 +1,16 @@
+#Written by Liam Henry 24/11/2021
 import unittest
 from main import CSV
-from main import Records
+from main import Record
 from main import sortArray
 test_array = []
 
-#A test to verify the CSV function has added objects of the type Records into the array
+#A test to verify the CSV function has added objects of the type Record into the array
 class Test_main_Functions(unittest.TestCase):	
 	def test_CSV(self): 
 		CSV(test_array)
 		for i in range(len(test_array)):
-			self.assertIsInstance(test_array[i], Records)
+			self.assertIsInstance(test_array[i], Record)
 	
 #A test to verify the array of objects is in the correct order
 	def test_sortArray(self): 
